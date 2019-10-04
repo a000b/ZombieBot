f23a61027a5241fb91ca695d49e0ce7dddf4108f
a000b <46565897+a000b@users.noreply.github.com>
2019-10-04 11:19:05 +0200

    Update fear.py

--- fear.py
+++ fear.py
@@ -2,7 +2,7 @@ import wypok_bot_lib
 
 def main():    
     w = wypok_bot_lib
-    url = "https://alternative.me/crypto/fear-and-greed-index."
+    url = "https://alternative.me/crypto/fear-and-greed-index"
     img = url + ".png"
     text = "Źródło : " + url
     w.add_entry(text, img)
