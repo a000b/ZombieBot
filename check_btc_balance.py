a89b0151667e4dfe4f6c331064697810fcaab611
a000b <46565897+a000b@users.noreply.github.com>
2019-11-08 00:37:12 +0100

    Check balance
    Signed-off-by: a000b <46565897+a000b@users.noreply.github.com>

---
+++ check_btc_balance.py
@@ -0,0 +1,99 @@
+# API w linku https://github.com/blockstream/esplora/blob/master/API.md
+
+import requests
+import pickle
+import pprint as pp
+import wypok_bot_lib as w
+
+
+addr_list = []
+addr_list.append("1BitcoinEaterAddressDontSendf59kuE")
+addr_list.append("1CounterpartyXXXXXXXXXXXXXXXUWLpVr")
+addr_list.append("1QLbz7JHiBTspS962RLKV8GndWFwi5j6Qr")
+addr_list.append("1111111111111111111114oLvT2")
+entry_list = []
+
+
+def getbalance(addresses):
+    entry = {}
+    entry_balance = {}
+    balance = 0
+    for addr in addresses:
+        try:
+            response = requests.get('https://blockstream.info/api/address/' + addr)
+        except Exception as e:
+            # print(e)
+            pass
+        else:
+            if response.status_code == 200:
+                content = response.json()
+                # pprint(content)
+                balance = (int(content['chain_stats']['funded_txo_sum']) - int(content['chain_stats']['spent_txo_sum'])) / 10**8
+                entry_balance = {'link' : 'https://blockstream.info/address/' + addr, 'balance' : balance}
+                txs_entry = getx(addr)
+                entry = {**entry_balance, **txs_entry}
+                entry_list.append(entry)
+            else:
+                # print(response.status_code)
+                pass
+    return entry_list
+
+def getx(address):
+    try:
+        response = requests.get('https://blockstream.info/api/address/' + address + "/txs")
+    except Exception as e:
+        # print(e)
+        pass
+    else:
+        if response.status_code == 200:
+            content = response.json()
+            entry_tx = {"last txid" : content[0]["txid"], 'block_height': content[0]["status"]["block_height"], "address" : address}
+            # print(entry)
+            # pprint(content[0])
+        else:
+            # print(response.status_code)
+            pass
+    return entry_tx
+
+
+def save_file(dane):
+    with open("balance.pickle", "wb") as plik:
+        pickle.dump(dane, plik)
+
+
+def read_file():
+    list_read = []
+    with open("balance.pickle", "rb") as plik:
+        list_read = pickle.load(plik)
+    return list_read
+
+def find_text(search_string):
+    existing_list = read_file()
+    for k, s in enumerate(existing_list):
+        found = str(s).find(search_string)
+        if found != -1:
+            return s['block_height']
+        else:
+            pass
+
+
+def main():
+    lista_wpis = []
+    mseg = "Rysiek balance check test:\n\n"
+    entries = getbalance(addr_list)
+
+    if len(entries) != 0:
+        for entry in entries:
+            if entry['block_height'] == find_text(entry["address"]):
+                # print(entry)
+                lista_wpis.append(entry)
+        if len(lista_wpis) != 0:
+            mesg = pp.pformat(lista_wpis)
+            print("test", mesg)
+            img = ''
+            w.add_entry(mseg, img)
+    else:
+        print('err')
+    save_file(entries)
+
+main()
