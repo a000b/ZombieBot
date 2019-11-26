46331324413092ffebdc1339aef6b7d8823ade6b
a000b <46565897+a000b@users.noreply.github.com>
2019-11-26 16:17:33 +0100

    add logging

--- bakkt_mesg.py
+++ bakkt_mesg.py
@@ -1,6 +1,11 @@
 import wypok_bot_lib
 import bakkt_lib
+import logging
 
+
+target_path = ""
+logging.basicConfig(filename=target_path + 'logs.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(filename)s:%(funcName)s:%(message)s')
 
 
 def new_entry(*args):
@@ -8,28 +13,27 @@ def new_entry(*args):
     mesg = f"BAKKT stats:\n\n"
     for i in range(0, len(data), 6):
         z = data[i:i + 6]
-        mesg += (
-        f"Kontrakt : {z[0]}\n"
-        f"Wolumen : {z[4]} BTC\n"
-        f"Czas : {z[2]}\n"
-        f"Cena : {z[1]}\n"
-        f"Zmiana : {z[3]}%\n\n"
-        )
-        
-    mesg += (
-        f"Źródło : {args[0][1]}\n\n"
-        )
+        try:
+            if len(z[4]) > 0:
+                mesg += (f"Kontrakt : {z[0]}\n" 
+                         f"Wolumen : {z[4]} BTC\n"
+                         f"Czas : {z[2]}\n"
+                         f"Cena : {z[1]}\n"
+                         f"Zmiana : {z[3]}%\n\n")
+            else:
+                logging.WARNING(f"Kontrakt : {z[0]}: Brak danych")
+        except Exception as e:
+            logging.error(f"{e}")
 
+    mesg += (f"Źródło : {args[0][1]}\n\n")
     return(mesg)
 
 def main():
     b = bakkt_lib.get_info('monthly')
     if b != 'err':
-#         print(new_entry(b))
         img = ''
-        wypok_bot_lib.add_entry((new_entry(b), img))
+        wypok_bot_lib.add_entry(new_entry(b), img)
     else:
-        print('err')
+        pass
 
 main()
-
