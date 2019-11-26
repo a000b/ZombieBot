d91ed88cbfd2c0c3d3707d1c32bf4fc534c4dba5
a000b <46565897+a000b@users.noreply.github.com>
2019-11-26 14:29:27 +0100

    add logging

--- wypok_bot_lib.py
+++ wypok_bot_lib.py
@@ -2,7 +2,10 @@ import requests
 import json
 import os
 import wypok_auth as w
+import logging
 
+logging.basicConfig(filename='logs.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(filename)s:%(funcName)s:%(message)s')
 
 def load_parms():
     target_path = ""
@@ -10,19 +13,17 @@ def load_parms():
     parms = w.load_file(target_path + pickle_f)
     if w.check_usrkey_isvalid(parms) == False:
         w.update_usr_key(target_path + pickle_f, parms)
-    print(parms)
     return parms
 
 
 
-def add_entry(text, img, mode=0):
+def add_entry(text, img='', mode=0):
     my_pickle = load_parms()
     podpis = "\n\nhttps://github.com/a000b/ZombieBot/blob/master/check_btc_balance.py\n" \
             "https://github.com/a000b/ZombieBot\n\n"
     tagi = "#bitcoin #kryptowaluty #zombiebot"
     entry = text + podpis + tagi
     url = f"https://a2.wykop.pl/Entries/Add/appkey/{my_pickle['appkey']}/token/{my_pickle['usrkey']}/userkey/{my_pickle['usrkey']}/"
-    print(url)
 
     if mode == 0:
         data = {'body': entry,
@@ -30,17 +31,18 @@ def add_entry(text, img, mode=0):
         tajny = f"{my_pickle['secret']}{url}{entry},{img}"
         try:
             r = requests.post(url, data=data, headers=w.sign_data(tajny))
-            print(r.text)
-        except:
-            print(r.json())
+            logging.info(f"{text[:5]}")
+        except Exception as e:
+            logging.error(f"{e}{text[:5]}")
     else:
         myfiles = {'embed': (img, open(img ,'rb'), 'image/png')}
         data = {'body': entry}
         tajny = f"{my_pickle['secret']}{url}{entry}"
         try:
             r = requests.post(url, data=data, files=myfiles, headers=w.sign_data(tajny))
-        except:
-            print(r.json())
+            logging.info(f"{text[:5]}")
+        except Exception as e:
+            logging.error(f"{e}{text[:5]}")
 
 
 
