b9968de9f18b501a1cd78b918fc7ee1155072cae
a000b <46565897+a000b@users.noreply.github.com>
2020-01-25 22:26:30 +0100

    response module refactoring

--- response2comments.py
+++ response2comments.py
@@ -33,6 +33,7 @@ def get_notifications():
             except Exception as e:
                 commentid = ""
                 type = 2
+
             if new == True:
 
                 if type == 1:
@@ -42,38 +43,38 @@ def get_notifications():
                     logging.info(f'{id} {user} {post} ')
                     replay = parse_comment(w.get_entry(post))
 
-                if replay == None:
-                    search_wypok = cs.parse_response(cs.google_search_wypok(cleanbody))
-                    if search_wypok != "err":
-                        entry = f'Dziękuję za komentarz/pytanie.\n' \
-                                 f'Niestety nie rozumiem jego treści.\n' \
-                                 f'Może tu znajdziesz odpowiedzi:\n' \
-                                 f'{search_wypok}\n' \
-                                 f'Serdecznie pozdrawiam :)'
-                    else:
-                        entry = f'{user}: Dziękuję za komentarz/pytanie.\n' \
-                            f'Niestety nie rozumiem jego treści.\n' \
-                            f'Serdecznie pozdrawiam :)'
-                elif replay == "cyberreturn":
+                if replay[0] == "cyberreturn":
                     days = calculate_remining_days(2020, 3, 7)
                     entry = f"@cyberpunkbtc wraca za {days} dni"
-                elif replay == "halving":
+                elif replay[0] == "halving":
                     info = halving.get_halving_info()
                     entry = info["hinfo"]
-                elif replay == "btc price":
+                elif replay[0] == "btc price":
                     current_pricebtc = get_ask_price("BTC-USD")
                     if current_pricebtc != "err":
                         entry = f'Ostania cena ask BTC na coinbase pro: {current_pricebtc} $ '
                     else:
                         entry = f'Coś poszło nie tak, nie mogę ściągnąć ceny ;( '
-                elif replay == "eth price":
+                elif replay[0] == "eth price":
                     current_priceeth = get_ask_price("ETH-USD")
                     if current_priceeth != "err":
                         entry = f'Ostania cena ask ETH na coinbase pro: {current_priceeth} $ '
                     else:
                         entry = f'Coś poszło nie tak, nie mogę ściągnąć ceny ;( '
+                elif  replay[0] == None:
+                    answer = cs.parse_response(cs.google_search_wypok(replay[1]))
+                    if replay != "err":
+                        entry = f'Dziękuję za komentarz/pytanie.\n' \
+                                f'Niestety nie rozumiem jego treści.\n' \
+                                f'Może tu znajdziesz odpowiedzi:\n' \
+                                f'{answer}\n' \
+                                f'Serdecznie pozdrawiam :)'
+                    else:
+                        entry = f'{user}: Dziękuję za komentarz/pytanie.\n' \
+                                f'Niestety nie rozumiem jego treści.\n' \
+                                f'Serdecznie pozdrawiam :)'
                 else:
-                    entry = f'{user}: {replay}'
+                    entry = f'{user}: {replay[0]}'
                 w.add_comments(user, post, entry, img='')
     else:
         logging.info(f'Error podczas czytania powiadomien')
@@ -87,7 +88,8 @@ def parse_comment(comment):
     cleanbody = newbody.replace("@atari_xe:","").replace("#bitcoin", "").\
         replace("#kryptowaluty", "").replace("#zombiebot","").\
         replace(":", "").strip()
-    return answer_dic.get_answer(cleanbody)
+    answer = answer_dic.get_answer(cleanbody)
+    return (answer, cleanbody)
 
 
 def main():
