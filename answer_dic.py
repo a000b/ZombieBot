a472136698c72b004d7501e2f39503d38e77c10f
a000b <46565897+a000b@users.noreply.github.com>
2020-02-26 07:17:22 +0100

    refactoring

--- answer_dic.py
+++ answer_dic.py
@@ -1,8 +1,9 @@
+import unicodedata
 from typing import Dict
 import logging
 
-target_path: str = ""
-target_path = ""
+target_path: str = "/home/maciej/myfiles/"
+
 logging.basicConfig(filename=target_path + 'logs.log', level=logging.INFO,
                     format='%(asctime)s|%(levelname)s|%(filename)s|%(funcName)s|%(message)s')
 
@@ -13,21 +14,21 @@ def add_to_dict() -> dict:
     :return: dict
     """
     ANSWERS_DICT: Dict[str, str] = dict()
-    ANSWERS_DICT['cześć'] = "Cześć :)"
+    ANSWERS_DICT['czesc'] = "Cześć :)"
     ANSWERS_DICT['hej'] = "Hej :)"
-    ANSWERS_DICT['co słychać'] = "Nie wiem nie mam uszu"
+    ANSWERS_DICT['co slychac'] = "Nie wiem nie mam uszu"
     ANSWERS_DICT['gdzie jest cyber'] = "Cyber dzban dostał ban XD"
-    ANSWERS_DICT['kim jesteś'] = "Jestem prostym botem"
-    ANSWERS_DICT['pokaż cycki'] = "Nie :P"
-    ANSWERS_DICT['gdzie jest loża'] = "Na junta.pl"
-    ANSWERS_DICT['gdzie jesteś'] = "W chmurce Google"
+    ANSWERS_DICT['kim jestes'] = "Jestem prostym botem"
+    ANSWERS_DICT['pokaz cycki'] = "Nie :P"
+    ANSWERS_DICT['gdzie jest loza'] = "Na junta.pl"
+    ANSWERS_DICT['gdzie jestes'] = "W chmurce Google"
     ANSWERS_DICT['gdzie mieszkasz'] = "W chmurce Google"
     ANSWERS_DICT['ile masz lat'] = "Kilka miesięcy :)"
     ANSWERS_DICT['gdzie jest pawlo'] = "W blokchainie"
     ANSWERS_DICT['ceo bitcoina'] = "Pawlo74 jest CEO BTC"
     ANSWERS_DICT['ceo btc'] = "Pawlo74 jest CEO BTC"
     ANSWERS_DICT['spierdalaj'] = "u2 :)"
-    ANSWERS_DICT['ruchasz się'] = ":P nie wiem o co chodzi"
+    ANSWERS_DICT['ruchasz sie'] = ":P nie wiem o co chodzi"
     ANSWERS_DICT['cenę btc'] = "btc price"
     ANSWERS_DICT['cene btc'] = "btc price"
     ANSWERS_DICT['cena btc'] = "btc price"
@@ -35,20 +36,26 @@ def add_to_dict() -> dict:
     ANSWERS_DICT['cene eth'] = "eth price"
     ANSWERS_DICT['cena eth'] = "eth price"
     ANSWERS_DICT['wraca cyber'] = "cyberreturn"
-    ANSWERS_DICT['powrót cybera'] = "cyberreturn"
-    ANSWERS_DICT['powrót cyberzejba'] = "cyberreturn"
+    ANSWERS_DICT['powrot cybera'] = "cyberreturn"
     ANSWERS_DICT['halving btc'] = "halving"
     ANSWERS_DICT['halwing btc'] = "halving"
     ANSWERS_DICT['halving bitcoina'] = "halving"
     ANSWERS_DICT['halwing bitcoina'] = "halving"
-    ANSWERS_DICT['dziękuję'] = "nie ma za co :)"
+    ANSWERS_DICT['dziekuje'] = "nie ma za co :)"
     ANSWERS_DICT['nowe konto eth'] = "newethaccount"
-    ANSWERS_DICT['cena złota'] = "goldprice"
+    ANSWERS_DICT['cena zlota'] = "goldprice"
     ANSWERS_DICT['gold price'] = "goldprice"
-    ANSWERS_DICT['ile kosztuje złoto'] = "goldprice"
-    ANSWERS_DICT['po ile złoto'] = "goldprice"
-    ANSWERS_DICT['cenę złota'] = "goldprice"
+    ANSWERS_DICT['ile kosztuje zloto'] = "goldprice"
+    ANSWERS_DICT['po ile zloto'] = "goldprice"
+    ANSWERS_DICT['cene zlota'] = "goldprice"
     return ANSWERS_DICT
+
+
+def remove_polish_chars(text: str) -> str:
+    """ Changing to ascii characters"""
+    return str(unicodedata.normalize('NFKD', text)
+               .replace(u'ł', 'l').replace(u'Ł', 'L').
+               encode('ascii', 'ignore'))
 
 
 def get_answer(question: str) -> str:
@@ -56,6 +63,7 @@ def get_answer(question: str) -> str:
     Searching the dict using given str
     :return: str
     """
+    question = remove_polish_chars(question)
 
     try:
         for key, value in add_to_dict().items():
@@ -63,8 +71,8 @@ def get_answer(question: str) -> str:
                 return  f'{value}'
         else:
             return  f'Not found'
-    except:
+    except Exception as e:
+        logging.error(e)
         return f'Not found'
-
 
 
